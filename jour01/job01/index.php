d41de0d4045452e9c7e1921d958a8360d801a472
Rémy DELPECH <remy.delpech@laplateforme.io>
2023-05-22 11:29:57 +0200

    Ajout du job 00 et 01

---
+++ jour01/job01/index.php
@@ -0,0 +1,29 @@
+<?php
+    // Création de la variable str et affectation de sa valeur
+    $str = "LaPlateforme";
+    echo $str;  // Affichage du contenu de str
+    echo "<br>";  // Saut de ligne
+
+    // Création des variables str2 et str3 et affectation de leurs valeurs
+    $str2 = "Vive";
+    $str3 = "!";
+    echo $str2 . " " . $str . " " . $str3;  // Affichage en concaténant les variables
+    echo "<br>";  // Saut de ligne
+
+    // Création de la variable val et affectation de sa valeur
+    $val = 6;
+    echo $val;  // Affichage du contenu de val
+    echo "<br>";  // Saut de ligne
+
+    $val = $val + 4;
+    echo $val;  // Affichage du contenu de val après l'ajout de 4
+    echo "<br>";  // Saut de ligne
+
+    // Création de la variable myBool et affectation de sa valeur
+    $myBool = true;
+    echo $myBool;  // Affichage du contenu de myBool
+    echo "<br>";  // Saut de ligne
+
+    $myBool = false;
+    echo $myBool;  // Affichage du contenu de myBool après l'affectation de false
+?>
